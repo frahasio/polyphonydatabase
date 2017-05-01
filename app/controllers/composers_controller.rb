494e3e2069c6831e5d6663286ccdca275b287147
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2017-05-01 14:14:59 +0100

    Proper composer edit form

--- app/controllers/composers_controller.rb
+++ app/controllers/composers_controller.rb
@@ -1,6 +1,5 @@
 class ComposersController < ApplicationController
   def new
-
   end
 
   def create
@@ -10,7 +9,14 @@ class ComposersController < ApplicationController
   end
 
   def edit
+    @composer = Composer.find(params[:id])
+  end
 
+  def update
+    composer = Composer.find(params[:id])
+    composer.update_attributes(composer_params)
+
+    redirect_to new_composer_path
   end
 
 private
