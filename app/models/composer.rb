19794ed8f2e1838b08b5756ad7b8fc7e59a386b5
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2017-07-02 10:20:19 +0100

    Validate composer years

--- app/models/composer.rb
+++ app/models/composer.rb
@@ -3,4 +3,17 @@ class Composer < ActiveRecord::Base
   has_many :attributions, inverse_of: :composer
 
   validates :name, uniqueness: true
+
+  validate :years_are_valid
+
+  def years_are_valid
+    [
+      :from_year,
+      :to_year,
+    ].each do |field|
+      unless field.to_s =~ /^\d{4}$/
+        errors.add(field, :invalid, "must be a 4-digit year")
+      end
+    end
+  end
 end
