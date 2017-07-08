7bee3e6398c3803d38beece1d8fa9807d0be7018
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2017-07-08 12:59:53 +0100

    Delete empty recordings and editions

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,7 +1,7 @@
 class Piece < ActiveRecord::Base
   has_many :inclusions
   has_many :sources, through: :inclusions
-  has_many :recordings
+  has_many :recordings, dependent: :destroy, autosave: true
   has_many :editions
 
   accepts_nested_attributes_for :recordings, reject_if: :unfilled?
@@ -11,6 +11,8 @@ class Piece < ActiveRecord::Base
 
   serialize :feasts
 
+  before_save :mark_blanks_for_deletion
+
   def composers
     inclusions.flat_map(&:composers)
   end
@@ -19,5 +21,19 @@ private
 
   def unfilled?(attrs)
     attrs.values.all?(&:blank?)
+  end
+
+  def mark_blanks_for_deletion
+    editions.each do |edition|
+      if edition.attributes.slice("editor", "voicing", "file_url").values.all?(&:blank?)
+        edition.mark_for_destruction
+      end
+    end
+
+    recordings.each do |recording|
+      if recording.attributes.slice("performer", "file_url").values.all?(&:blank?)
+        recording.mark_for_destruction
+      end
+    end
   end
 end
