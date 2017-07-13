a2de82068e7fce6241c830e17ff7e51d9f180730
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2017-07-13 21:07:53 +0100

    Fix possible nil bug

--- app/controllers/inclusions_controller.rb
+++ app/controllers/inclusions_controller.rb
@@ -1,9 +1,11 @@
 class InclusionsController < ApplicationController
   def index
     @grouped_inclusions = Inclusion.all.group_by do |inclusion|
+      composer_list = inclusion.composers.pluck(:id)&.join(",") || ""
+
       UniquePiece.find_or_initialize_by(
         title: inclusion&.piece&.title,
-        composers: inclusion.composers.pluck(:id).join(","),
+        composers: composer_list,
         minimum_voices: inclusion.minimum_voice_count,
       )
     end
