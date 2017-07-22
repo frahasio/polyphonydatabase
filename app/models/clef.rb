13a69ef1f1a3cafe1fb8f7ad00ed77a45a65be09
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2017-07-22 11:48:55 +0100

    Downcase clefs

--- app/models/clef.rb
+++ app/models/clef.rb
@@ -1,4 +1,8 @@
 class Clef < ActiveRecord::Base
   has_many :clefs_inclusions, inverse_of: :clef
   has_many :inclusions, through: :clefs_inclusions
+
+  def note=(note_string)
+    super(note_string.downcase)
+  end
 end
