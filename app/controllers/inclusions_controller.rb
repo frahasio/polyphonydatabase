8877b03eb94b976a3ce458ac97562874ede95008
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2017-07-25 21:19:28 +0100

    Add searching in recordings and editions

--- app/controllers/inclusions_controller.rb
+++ app/controllers/inclusions_controller.rb
@@ -24,16 +24,7 @@ private
   def search(term, unique_piece, inclusions)
     return true if term.blank?
 
-    [
-      unique_piece.title,
-      inclusions.first.composers.compact.map(&:aliased_as).join(" | "),
-      inclusions.first.composers.compact.map(&:name).join(" | "),
-      inclusions.map(&:source).compact.map(&:title).join(" | "),
-      inclusions.map(&:source).compact.map(&:code).join(" | "),
-      inclusions.map(&:source).compact.map(&:location_and_pubscribe).join(" | "),
-      inclusions.map(&:source).compact.map(&:dates).join(" | "),
-      inclusions.map(&:source).compact.map(&:type).join(" | "),
-    ].any? { |string| string.include?(term) }
+    Searcher.new(unique_piece, inclusions).search(term)
   end
 
   def function(function_code, unique_piece)
