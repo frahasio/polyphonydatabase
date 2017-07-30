51b433158157c746847789e6ac75c4e6c35d87b5
frahasio <30375921+frahasio@users.noreply.github.com>
2017-07-30 19:39:01 +0100

    Update inclusion.rb

--- app/models/inclusion.rb
+++ app/models/inclusion.rb
@@ -41,7 +41,7 @@ class Inclusion < ActiveRecord::Base
   end
 
   def minimum_voice_count
-    clefs_inclusions.where(partial: false).where.not(clefs.note: ['bc','lut','org']).count
+    clefs_inclusions.where(partial: false).joins(:clef).where.not(clefs: {note: ['bc','lut','org']}).count
   end
 
   def public_notes
