b26c963a12745ed214572f35135c0172bc273655
frahasio <30375921+frahasio@users.noreply.github.com>
2017-09-18 18:30:45 +0100

    Update jquery.formnavigation.js

--- app/assets/javascripts/jquery.formnavigation.js
+++ app/assets/javascripts/jquery.formnavigation.js
@@ -6,16 +6,16 @@
 (function ($) {
     $.fn.formNavigation = function () {
         $(this).each(function () {
-            $(this).find('input').on('keyup', function(e) {
+            $(this).find('textarea').on('keyup', function(e) {
                 switch (e.which) {
                     case 39:
-                        $(this).closest('td').next().find('input').focus(); break;
+                        $(this).closest('td').next().find('textarea').focus(); break;
                     case 37:
-                        $(this).closest('td').prev().find('input').focus(); break;
+                        $(this).closest('td').prev().find('textarea').focus(); break;
                     case 40:
-                        $(this).closest('tr').next().children().eq($(this).closest('td').index()).find('input').focus(); break;
+                        $(this).closest('tr').next().children().eq($(this).closest('td').index()).find('textarea').focus(); break;
                     case 38:
-                        $(this).closest('tr').prev().children().eq($(this).closest('td').index()).find('input').focus(); break;
+                        $(this).closest('tr').prev().children().eq($(this).closest('td').index()).find('textarea').focus(); break;
                 }
             });
         });
