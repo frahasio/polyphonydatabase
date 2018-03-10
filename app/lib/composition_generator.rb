514b0b0b11d70978cd63034350b31df04a3a8d78
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-03-10 18:36:36 +0000

    Assign anon composer when generating compositions

--- app/lib/composition_generator.rb
+++ app/lib/composition_generator.rb
@@ -1,14 +1,17 @@
 class CompositionGenerator
   def self.run
     to_check_composer = Composer.find_or_create_by!(name: "to check")
+    anon_composer = Composer.find_by!(name: "Anon")
 
     UniquePiece.all.order(:id).each do |unique_piece|
       title = Title.find_or_create_by!(text: unique_piece.title)
       number_of_voices = unique_piece.minimum_voices
 
       composers = unique_piece.inclusions.flat_map(&:attributions).map {|a|
-        if a.incorrectly_attributed? || a.alias.nil?
+        if a.incorrectly_attributed?
           to_check_composer
+        elsif a.alias.nil?
+          anon_composer
         else
           a.alias.composer
         end
