b37e77f6711e818f273b5f374d2c106297d44f3b
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-03-10 18:36:45 +0000

    Groups are optional for compositions

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -1,6 +1,6 @@
 class Composition < ApplicationRecord
   belongs_to :title
-  belongs_to :group
+  belongs_to :group, optional: true
   has_many :inclusions
   has_and_belongs_to_many :composers
 end
