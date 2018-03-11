d5ca93441d31f78a27432e727b1f26b951a977da
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-03-11 15:59:43 +0000

    Support group merging

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class GroupsController < ApplicationController
     def index
-      groups = Group.distinct(:id).order(:display_title)
+      groups = Group.distinct.order(:display_title)
       groups = GroupFilter.new(params).filter(groups)
       groups = groups.includes(
         compositions: [
@@ -16,7 +16,30 @@ module Admin
     end
 
     def merge
-      redirect_to admin_groups_path
+      if params[:groups].blank?
+        flash[:error] = "You have to select groups to merge"
+        redirect_to admin_groups_path(request.query_parameters)
+        return
+      end
+
+      groups = Group.find(params[:groups])
+
+      compositions = groups.flat_map(&:compositions)
+      editions = groups.flat_map(&:editions)
+      recordings = groups.flat_map(&:recordings)
+
+      new_group = groups.shift
+
+      new_group.display_title = params[:display_title] unless params[:display_title].blank?
+      new_group.update!(
+        compositions: compositions,
+        editions: editions,
+        recordings: recordings,
+      )
+
+      groups.each(&:destroy)
+
+      redirect_to admin_groups_path(request.query_parameters)
     end
   end
 end
