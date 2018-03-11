c96c50eee5af3dd41685e8005f1f45d99ced485d
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-03-11 13:27:44 +0000

    Don't allow different voice counts in groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,14 @@ class Group < ApplicationRecord
   has_many :compositions
   has_many :editions
   has_many :recordings
+
+  validate :matching_voices
+
+private
+
+  def matching_voices
+    if compositions.pluck(:number_of_voices).uniq.count > 1
+      errors.add(:base, "All compositions in the group must have the same number of voices")
+    end
+  end
 end
