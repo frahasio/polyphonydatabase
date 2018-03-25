d9b053924dd11276a031f47ce3f07da0fd3af3c7
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-03-25 20:13:48 +0100

    Add title filtering by letter

--- app/controllers/admin/titles_controller.rb
+++ app/controllers/admin/titles_controller.rb
@@ -1,12 +1,15 @@
 module Admin
   class TitlesController < AdminControllerBase
     def index
-      @titles = Title
+      titles = Title
         .includes(:functions)
         .select("titles.*, count(inclusions.id) as inclusions_count")
           .joins(compositions: :inclusions)
           .group("titles.id")
         .order(:text)
+
+      @titles = filter_by_letter(titles)
+
       @functions = Function.order(:name)
     end
 
@@ -27,6 +30,34 @@ module Admin
         :text,
         function_ids: [],
       )
+    end
+
+    def current_letter
+      params[:letter] || special_character
+    end
+    helper_method :current_letter
+
+    def letter_options
+      @letter_options ||= [special_character].concat(("A".."Z").to_a)
+    end
+    helper_method :letter_options
+
+    def special_character
+      "#"
+    end
+
+    def valid_letter?
+      current_letter.blank? || letter_options.include?(current_letter)
+    end
+
+    def filter_by_letter(titles)
+      raise RuntimeError.new("Invalid letter page") unless valid_letter?
+
+      if current_letter == special_character
+        titles.where("text ~* '^[^a-z]'")
+      else
+        titles.where("text ~~* '#{current_letter}%'")
+      end
     end
   end
 end
