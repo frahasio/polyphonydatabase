02674fb4dc746f16fbcf3f6a137885fa58a8a532
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-03-31 14:47:36 +0100

    Add better profiling options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def profile
+    RubyProf.start
+
+    yield
+
+    result = RubyProf.stop
+
+    file = File.open("profile.html", "w")
+    RubyProf::CallStackPrinter.new(result).print(file)
+    file.close
+  end
 end
