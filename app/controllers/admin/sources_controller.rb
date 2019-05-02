89bdd91d37307de4874d6dc57a70b9ecd256d30a
frahasio <30375921+frahasio@users.noreply.github.com>
2019-05-02 15:53:40 +0100

    Update sources_controller.rb

--- app/controllers/admin/sources_controller.rb
+++ app/controllers/admin/sources_controller.rb
@@ -6,7 +6,7 @@ module Admin
     def edit
       @source = Source.find(params[:id])
 
-      @grouped_sources = Sources.order(:code) {
+      @grouped_sources = {
         "Catalogued" => Source.catalogued.pluck(:code, :id),
         "Uncatalogued" => Source.uncatalogued.pluck(:code, :id),
       }
