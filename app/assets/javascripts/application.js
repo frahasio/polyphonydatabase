800e5775439713e42789cdf3e184cb277a68977d
frahasio <30375921+frahasio@users.noreply.github.com>
2021-04-09 00:42:48 +0100

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -111,15 +111,6 @@ $(document).keydown(
 			if (e.keyCode == 40) {  //MOVE DOWN
 				$('.body-row').eq(par + 1).find('.notes').children('textarea').focus();
 			}
-		} else if ($(':focus').closest('div').hasClass('attributed-to')) {
-			if ($(':focus').is('input') {
-				if (e.keyCode == 38) {  //MOVE UP
-					$('.body-row').eq(par - 1).find('.attributed-to').find('li').first().find('input[type=text]').focus();
-				}
-				if (e.keyCode == 40) {  //MOVE DOWN
-					$('.body-row').eq(par + 1).find('.attributed-to').find('li').first().find('input[type=text]').focus();
-				}
-			}
 		}
     }
 );
