0f85083fb3b644b70137ead86a3ade1ef7bea2fc
frahasio <30375921+frahasio@users.noreply.github.com>
2021-04-22 22:27:42 +0100

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,7 @@ class GroupsController < ApplicationController
   def index
     @groups = GroupFilter.filter(params)
       .order(:display_title)
+      .limit(200)
       .includes(
         :recordings,
         :functions,
