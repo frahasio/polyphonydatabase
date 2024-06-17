8de78632cd2064a789dcffe81ae100e497f69c8b
frahasio <30375921+frahasio@users.noreply.github.com>
2024-06-17 09:27:55 +0100

    Update bevan.js

--- app/assets/javascripts/bevan.js
+++ app/assets/javascripts/bevan.js
@@ -12,18 +12,13 @@ $( document ).ready(function(){
 	    	var rowIndex = $(this).closest('.body-row').index('.body-row');
 	    	var tally = 0;
 	    	var inputsToUpdate = $('.body-row').eq(rowIndex).find('.voice-count input');
-		var compositionDiv = $('.body-row').eq(rowIndex).find('.col.composition');
-		console.log(rowIndex);
 		$('.body-row').eq(rowIndex).find('.clefs input').each(function(i,e) {
-			console.log($(e).val());
-			console.log($(e).attr('type'));
 			if (!$(e).val() || $(e).attr('type') == 'hidden' || $(e).val().indexOf("(") !== -1 || $(e).val().indexOf(")") !== -1 || $(e).val().indexOf("bc") !== -1 || $(e).val().indexOf("lut") !== -1 || $(e).val().indexOf("org") !== -1) {
 				// do nothing
 			} else {
 				tally = tally + 1;
 			};
 		});
-		console.log(tally);
 		inputsToUpdate.val(tally);
 		inputsToUpdate.trigger('focusout');
 	});
