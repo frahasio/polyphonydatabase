7feb3d6f3156f5f7b1715548963f13aef42ef48b
frahasio <30375921+frahasio@users.noreply.github.com>
2024-06-17 10:24:19 +0100

    Update bevan.js

--- app/assets/javascripts/bevan.js
+++ app/assets/javascripts/bevan.js
@@ -11,7 +11,7 @@ $( document ).ready(function(){
 	$('.clefs input').on('change keydown', function(e){
 	    	var rowIndex = $(this).closest('.body-row').index('.body-row');
 	    	var tally = 0;
-	    	var inputToUpdate = $('.body-row').eq(rowIndex).find('.voice-count input');
+	    	var inputToUpdate = $('.body-row').eq(rowIndex).find('.voice-count input[data-composition-target="numberOfVoices]');
 		$('.body-row').eq(rowIndex).find('.clefs input').each(function(i,e) {
 			if (!$(e).val() || $(e).attr('type') == 'hidden' || $(e).val().indexOf("(") !== -1 || $(e).val().indexOf(")") !== -1 || $(e).val().indexOf("bc") !== -1 || $(e).val().indexOf("lut") !== -1 || $(e).val().indexOf("org") !== -1) {
 				// do nothing
