3fad042ea36de5c202ba88d9cc66b14849ba7783
frahasio <30375921+frahasio@users.noreply.github.com>
2025-06-26 16:20:10 +0100

    Update changes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import pool from '../db.js';
+import { pool } from '../db.js';
 import { requireAdmin } from '../middleware/auth.js';
 
 const router = express.Router();
